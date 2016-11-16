39c3b62c451471aa7ee594a4fcaaa0ecfce8884f
parazyd <parazyd@dyne.org>
2016-11-16 12:32:59 +0100

    add work so far done on the rootfs overlay

---
+++ etc/icedove/pref/icedove.js
@@ -0,0 +1,26 @@
+
+pref("extensions.update.enabled", false);
+
+pref("intl.locale.matchOS", true);
+
+pref("mail.shell.checkDefaultMail", false);
+
+pref("network.protocol-handler.app.http", "x-www-browser");
+pref("network.protocol-handler.app.https", "x-www-browser");
+
+pref("mailnews.database.global.indexer.enabled", false);
+
+pref("mail.chat.enabled", false);
+
+pref("extensions.autoDisableScopes", 3);
+pref("extensions.enabledScopes", 4);
+
+pref("mail.tabs.autoHide", true);
+
+pref("toolkit.telemetry.prompted", 2);
+pref("toolkit.telemetry.rejected", true);
+pref("toolkit.telemetry.enabled", false);
+
+pref("mailnews.auto_config.ssl_only", true);
+pref("mailnews.auto_config.oauth2.enabled", false);
+pref("mailnews.auto_config.dns_mx_lookup.enabled", false);
