78283d819274130854d0c3e5a6d42af7e348f295
parazyd <parazyd@dyne.org>
2017-02-22 13:54:32 +0100

    updates

--- etc/icedove/pref/icedove.js
+++ etc/icedove/pref/icedove.js
@@ -1,12 +1,11 @@
-
 pref("extensions.update.enabled", false);
 
 pref("intl.locale.matchOS", true);
 
 pref("mail.shell.checkDefaultMail", false);
 
-pref("network.protocol-handler.app.http", "x-www-browser");
-pref("network.protocol-handler.app.https", "x-www-browser");
+pref("network.protocol-handler.app.http", "icecat");
+pref("network.protocol-handler.app.https", "icecat");
 
 pref("mailnews.database.global.indexer.enabled", false);
 
@@ -15,7 +14,7 @@ pref("mail.chat.enabled", false);
 pref("extensions.autoDisableScopes", 3);
 pref("extensions.enabledScopes", 4);
 
-pref("mail.tabs.autoHide", true);
+//pref("mail.tabs.autoHide", true);
 
 pref("toolkit.telemetry.prompted", 2);
 pref("toolkit.telemetry.rejected", true);
